dc1bc374f540573d28ce71238949c79f520fdc22
birmasiveris <birmasiveris@hotmail.com>
2017-02-22 10:48:32 -0300

    Hipotenusa

--- Ponto.java
+++ Ponto.java
@@ -19,24 +19,23 @@ public class Ponto{
 	public static void main (String [] args){
 		System.out.println("Birmaaaaaa");
 		Ponto p1 = new Ponto();
-		p1.setX(10);
-		p1.setY(20);
+		p1.setX(5);
+		p1.setY(5);
 		System.out.println(p1.getX());
 		System.out.println(p1.getY());
 		Ponto p2 = new Ponto();
-		p2.setX(15);
-		p2.setY(30);
+		p2.setX(10);
+		p2.setY(10);
 		System.out.println(p2.getX());
 		System.out.println(p2.getY());
-		Ponto p3 = new Ponto();
-		p3.setX(50);
-		p3.setY(70);
-		p3 = p1;
-		System.out.println(p3.getX());
-		System.out.println(p3.getY());
-		Ponto p4 = p3;
-		System.out.println(p4.getX());
-		System.out.println(p4.getY());
+		double ca, co, h;
+		co = p2.getX() - p1.getX();
+		ca = p2.getY() - p1.getY();
+		co = co * co;
+		ca = ca * ca;
+		h = Math.sqrt(ca + co);
+		System.out.println(h);
+
 	}
 }
 
