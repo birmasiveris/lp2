df68d3ca85512b47332858fc11e8a6487efa74e7
birmasiveris <birmasiveris@gmail.com>
2017-10-04 08:32:57 -0300

    Criação FakeUser

---
+++ FakeUser/app/src/main/java/com/example/alunos/fakeuser/FakeUser.java
@@ -0,0 +1,109 @@
+package com.example.alunos.fakeuser;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by alunos on 04/10/17.
+ */
+
+public class FakeUser {
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String endereco;
+    private String cidade;
+    private String estado;
+    private String username;
+    private String senha;
+    private String nascimento;
+    private String telefone;
+    private Bitmap foto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Bitmap getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Bitmap foto) {
+        this.foto = foto;
+    }
+}
---
+++ FakeUser/app/src/main/java/com/example/alunos/fakeuser/Utils.java
@@ -0,0 +1,90 @@
+package com.example.alunos.fakeuser;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+
+/**
+ * Created by alunos on 04/10/17.
+ */
+
+public class Utils {
+    public FakeUser getInformacao(String end){
+        String json;
+        FakeUser retorno;
+        json = NetworkUtils.getJSONFromAPI(end);
+        Log.i("Resultado", json);
+        retorno = parseJson(json);
+
+        return retorno;
+    }
+
+    private FakeUser parseJson(String json){
+        try{
+            FakeUser pessoa = new FakeUser();
+
+            JSONObject jsonObj = new JSONObject(json);
+            JSONArray array = jsonObj.getJSONArray("results");
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Date data;
+
+            JSONObject objArray = array.getJSONObject(0);
+
+            JSONObject obj = objArray.getJSONObject("user");
+            //Atribui os objetos que estão nas camadas mais altas
+            pessoa.setEmail(obj.getString("email"));
+            pessoa.setUsername(obj.getString("username"));
+            pessoa.setSenha(obj.getString("password"));
+            pessoa.setTelefone(obj.getString("phone"));
+            data = new Date(obj.getLong("dob")*1000);
+            pessoa.setNascimento(sdf.format(data));
+
+            //Nome da pessoa é um objeto, estancia um novo JSONObject
+            JSONObject nome = obj.getJSONObject("name");
+            pessoa.setNome(nome.getString("first"));
+            pessoa.setSobrenome(nome.getString("last"));
+
+            //Endereco tambem é um objeto
+            JSONObject endereco = obj.getJSONObject("location");
+            pessoa.setEndereco(endereco.getString("street"));
+            pessoa.setEstado(endereco.getString("state"));
+            pessoa.setCidade(endereco.getString("city"));
+
+            //Imagem tb é um objeto
+            JSONObject foto = obj.getJSONObject("picture");
+            pessoa.setFoto(baixarImagem(foto.getString("large")));
+
+            return pessoa;
+        } catch(JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Bitmap baixarImagem(String url){
+        try{
+            URL endereco;
+            InputStream inputStream;
+            Bitmap imagem;
+            endereco = new URL(url);
+            inputStream = endereco.openStream();
+            imagem = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+            return imagem;
+        } catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
